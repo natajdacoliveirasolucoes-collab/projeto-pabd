10a0bbc93cb278b246ee5a7ac5ed2f67f1b17493
NatãOliveira <natajdacoliveira@gmail.com>
2026-04-14 21:56:44 -0400

    Initial commit

---
+++ DataContexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ApiFinanceiro.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiFinanceiro.DataContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Despesa> Despesas { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ Profiles/DespesaProfile.cs
@@ -0,0 +1,26 @@
+﻿using ApiFinanceiro.Dtos;
+using ApiFinanceiro.Models;
+using AutoMapper;
+
+namespace ApiFinanceiro.Profiles
+{
+    public class DespesaProfile : Profile
+    {
+        public DespesaProfile() 
+        {
+            CreateMap<DespesaDto, Despesa>()
+                .ForMember(
+                    dest => dest.Situacao,
+                    opt => opt.MapFrom(src => "pendente")
+                );
+
+            CreateMap<DespesaUpdateDto, Despesa>()
+                .ForMember(
+                    dest => dest.DataPagamento,
+                    opt => opt.MapFrom(
+                        src => src.DataPagamento.ToDateTime(TimeOnly.FromDateTime(DateTime.Now))
+                    )
+                );
+        }
+    }
+}
